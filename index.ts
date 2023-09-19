1d33548255e3cc3d301e6410791536d8a91bc215
sultanfariz <hydeblazack@gmail.com>
2023-09-19 08:43:42 +0700

    feat: init main app backbone

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import * as dotenv from 'dotenv';
+import app from './app';
+import config from './infrastructure/repository/mysql/drivers';
+import { Sequelize } from 'sequelize';
+
+dotenv.config();
+
+function normalizePort(port: string | undefined): number {
+  if (typeof port === 'string') {
+    return parseInt(port, 10);
+  } else if (typeof port === 'undefined') {
+    return 4000;
+  }
+  return port;
+}
+
+try {
+  const sequelize = config as Sequelize;
+
+  sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+
+    const port = normalizePort(process.env.PORT);
+    app.listen(port, () => console.log(`Application running on port ${port}`));
+  });
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
