c63988ca9fb2534970190c36dcbb3cb0cbfe758b
sultanfariz <hydeblazack@gmail.com>
2023-10-21 19:29:16 +0700

    fix: auth middleware library error

--- src/infrastructure/commons/middlewares/jwt.ts
+++ src/infrastructure/commons/middlewares/jwt.ts
@@ -1,13 +1,13 @@
-import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
+import jwt, { Secret } from 'jsonwebtoken';
 import { NotFoundError, UnauthorizedError } from '../exceptions';
 import { response } from '../response';
 import { Request, Response, NextFunction } from 'express';
 
 interface CustomRequest extends Request {
- user: string | JwtPayload | undefined;
+ user: string | undefined;
 }
 
-const auth = (req: Request, res: Response, next: NextFunction) => {
+const auth = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers?.['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
@@ -15,16 +15,15 @@ const auth = (req: Request, res: Response, next: NextFunction) => {
     if (!token)
       throw new NotFoundError('A token is required for authentication');
 
-      console.log(process.env.TOKEN);
-      console.log(token);
-
     jwt.verify(token, process.env.TOKEN || '', (err, decoded) => {
-      if (err)
-        throw new UnauthorizedError(
-          "Your token doesn't match our credentials"
-        );
-      console.log(decoded);
-      if (decoded) (req as CustomRequest).user = decoded;
+      if (err) {
+        throw new UnauthorizedError('Your token does not match our credentials');
+      }
+      if (decoded) {
+        res.locals.user = {
+          user: decoded,
+        };
+      }
     });
   } catch (error: any) {
     if (error.name === 'NotFoundError')
@@ -52,16 +51,16 @@ const auth = (req: Request, res: Response, next: NextFunction) => {
 
 const verifyRole = (roles: string[]) => {
   return (req: CustomRequest, res: Response, next: NextFunction) => {
-    // const role = req.user.role;
-    // if (roles.includes(role)) {
-    //   return next();
-    // } else {
-    //   return response(res, {
-    //     code: 403,
-    //     success: false,
-    //     message: 'You are not authorized to access this resource',
-    //   });
-    // }
+    const role = res.locals.user?.role;
+    if (roles.includes(role)) {
+      return next();
+    } else {
+      return response(res, {
+        code: 403,
+        success: false,
+        message: 'You are not authorized to access this resource',
+      });
+    }
   };
 };
 
