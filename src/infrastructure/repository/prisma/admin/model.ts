a7c0bd1ec323f2f96a41b9f65deac86f0e121e65
sultanfariz <hydeblazack@gmail.com>
2023-10-24 09:15:47 +0700

    feat(repository): add admin repository

---
+++ src/infrastructure/repository/prisma/admin/model.ts
@@ -0,0 +1,11 @@
+import { User } from "../user/model";
+
+type Admin = {
+  id: number | null;
+  userId: number | null;
+  user: User | null;
+  status: string | null;
+  lastLogin: Date | null;
+};
+
+export { Admin };
---
+++ src/infrastructure/repository/prisma/admin/repository.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const insertAdmin = async (data: any) => {
+  try {
+    const createdAdmin = await prisma.user.create({
+      data: data,
+    });
+
+    return createdAdmin;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+const updateAdmin = async (id: number, data: any) => {
+  try {
+    const updatedAdmin = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: data,
+    });
+
+    return updatedAdmin;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+const getAdminByUserId = async (userId: number) => {
+  try {
+    const admin = await prisma.admin.findFirst({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return admin;
+  } catch (error: any) {
+    throw error;
+  }
+}
+
+export { insertAdmin, updateAdmin, getAdminByUserId };
